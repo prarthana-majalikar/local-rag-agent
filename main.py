6b581929738509a0e2e43e2d1f1d1feebebe3535
Prarthana Majalikar <prarthana.majalikar@gmail.com>
2025-09-24 21:28:15 -0700

    Main setup, defining the model, prompt template, and chaining them

---
+++ main.py
@@ -0,0 +1,27 @@
+
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are an expert in answering questions about restaurants
+Here are some relevant reviews : {reviews}
+Here is the question to answer : {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = prompt | model
+
+while True:
+    print("\n\n=======================================")
+    question = input("Ask your question (q to quit) : ")
+    print("\n\n=======================================")
+    if question == "q":
+        break
+
+    result = chain.invoke({"reviews" : [], "question" : question})
+    print(result)
+
+
